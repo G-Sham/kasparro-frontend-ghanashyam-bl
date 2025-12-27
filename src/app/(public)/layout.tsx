6cf0127c4dce638e5fa6ad98e42330c9f6b610a2
GHANASHYAM BL <shubhalokesh123@gmail.com>
2025-12-27 05:37:40 +0000

    Nooo....all the contents are left aligned....change it to center

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -7,9 +7,9 @@ export default function PublicLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex min-h-screen flex-col items-center">
       <PublicHeader />
-      <main className="flex-1">{children}</main>
+      <main className="flex-1 w-full">{children}</main>
       <PublicFooter />
     </div>
   );
